01000a174703344a9c201e84d706394c85c079a2
munchiebarajas <miguelbarajas@live.se>
2023-05-22 21:39:17 +0200

    Basic stuff
    Added ds_store to gitignore, got a placeholder logo, created a quick landingpage with flask and html.

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def landing_page():
+    return render_template('landing.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
