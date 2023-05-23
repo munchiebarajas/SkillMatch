99b6a8adf8ab76843b0b047c49012b22508c5c62
munchiebarajas <miguelbarajas@live.se>
2023-05-23 22:06:18 +0200

    Created first four questions
    added logic for questions, saves input from user and functions are called from jobfinder.

--- questions.py
+++ questions.py
@@ -41,14 +41,93 @@ def get_selected_occupation_groups():
     return selected_occupations
 
 # Question 2: Do you have any previous working experience in Data/IT?
-#def ask_experience():
-    # Implement the code for this question
-    # ...
+def ask_experience():
+    while True:
+        user_input = input("Do you have any previous working experience in Data/IT? (yes/no): ").lower()
+        if user_input == "yes":
+            return True
+        elif user_input == "no":
+            return False
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
 
-# Question 3: Do you like programming? If yes, choose languages that you are familiar with and would like to work with.
-#def ask_programming_languages():
-    # Implement the code for this question
-    # ...
+# Question 3: What type of programming are you interested in?
+def ask_programming_type():
+    programming_types = {
+        1: "General-Purpose Programming",
+        2: "Web Development",
+        3: "Databases and Data Manipulation",
+        4: "Scientific and Mathematical Computing",
+        5: "Not interested in programming"
+    }
+
+    print("Programming Types:")
+    for i, programming_type in programming_types.items():
+        print(f"{i}. {programming_type}")
+
+    while True:
+        print("Enter the number(s) corresponding to your choice(s). Separate multiple choices by commas.")
+        user_input = input("Your choice(s): ").lower()
+
+        selected_types = []
+        selected_indexes = user_input.split(",")
+        selected_types = [programming_types[int(index.strip())] for index in selected_indexes if index.strip().isdigit()]
+
+        print("Selected programming type(s):")
+        for programming_type in selected_types:
+            print(programming_type)
+
+        confirm = input("Are these your final selections? (yes/no): ").lower()
+        if confirm == "yes":
+            break
+
+    print("Final selected programming type(s):")
+    for programming_type in selected_types:
+        print(programming_type)
+
+    return selected_types
+
+# Question 4: Select specific programming languages that you would like to work with
+def ask_specific_languages(programming_types):
+    specific_languages = []
+
+    for programming_type in programming_types:
+        if programming_type == "Not interested in programming":
+            break
+        elif programming_type == "General-Purpose Programming":
+            general_purpose_languages = ["Java", "Python", "C#", "TypeScript", "JavaScript", "C++", "Go", "C"]
+            specific_languages.extend(general_purpose_languages)
+        elif programming_type == "Web Development":
+            web_dev_languages = ["CSS", "HTML", "PHP"]
+            specific_languages.extend(web_dev_languages)
+        elif programming_type == "Databases and Data Manipulation":
+            specific_languages.append("SQL")
+        elif programming_type == "Scientific and Mathematical Computing":
+            sci_math_languages = ["MATLAB", "R"]
+            specific_languages.extend(sci_math_languages)
+
+    if specific_languages:
+        print("Specific programming languages:")
+        for i, language in enumerate(specific_languages, start=1):
+            print(f"{i}. {language}")
+        while True:
+            print("Enter the number(s) corresponding to your choice(s). Separate multiple choices by commas.")
+            user_input = input("Your choice(s): ").lower()
+            selected_indexes = user_input.split(",")
+            selected_languages = [specific_languages[int(index.strip()) - 1] for index in selected_indexes if index.strip().isdigit()]
+            if selected_languages:
+                break
+            else:
+                print("Invalid input. Please enter the number(s) corresponding to your choice(s).")
+
+        print("Selected programming language(s):")
+        for language in selected_languages:
+            print(language)
+        
+        return selected_languages
+    else:
+        print("No specific programming languages selected.")
+        return specific_languages
 
 # Additional questions...
 # ...
