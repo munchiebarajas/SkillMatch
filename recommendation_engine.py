5c9c3e2b53ace4af16becca10b3cbd7e36d7a9f8
munchiebarajas <miguelbarajas@live.se>
2023-05-25 01:33:51 +0200

    Update recommendation_engine.py
    Finished user preference and occupation profile (minus one function) methods. Tested and seems to be working.

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -1,4 +1,6 @@
 import json
+import re
+import pandas as pd
 from questions import get_selected_occupation_groups, ask_experience, ask_programming_type, ask_specific_languages
 
 class RecommendationEngine:
@@ -6,29 +8,12 @@ class RecommendationEngine:
         self.data = data
 
     def create_recommendation(self):
-        # Define question and answer weights
+        # Load question and answer weights from the JSON file
+        with open('weight_settings.json') as file:
+            weight_settings = json.load(file)
 
-        question_weights = {
-            "occupation_groups": 1.0,
-            "experience": 1.0,
-            "programming_type": 1.0,
-            "specific_languages": 1.0
-        }
-
-        answer_weights = {
-            "occupation_groups": {
-                "selected_occupations": 1.0
-            },
-            "experience": {
-                "has_experience": 1.0
-            },
-            "programming_type": {
-                "selected_types": 1.0
-            },
-            "specific_languages": {
-                "selected_languages": 1.0
-            }
-        }
+        question_weights = weight_settings['question_weights']
+        answer_weights = weight_settings['answer_weights']
 
         # Preset user inputs (for testing and development)
         selected_occupation_groups = ['Mjukvaru- och systemutvecklare m.fl.']
@@ -93,24 +78,117 @@ class Recommendation:
         return recommended_occupations
 
     def calculate_user_preferences(self):
-        # Implement the calculation of user preferences based on question/answer weights and user input
         # Calculate occupation group preferences based on selected occupation groups
-        occupation_group_preferences = {group: 1.0 if group in self.selected_occupation_groups else 0.0
-                                        for group in self.data['occupation_group_name']}
-        return occupation_group_preferences
+        occupation_group_preferences = {
+            group: self.question_weights['occupation_groups']
+            if group in self.selected_occupation_groups else 0.0
+            for group in set(self.data['occupation_group_name'])
+        }
 
+        # Calculate experience preferences based on work experience and education experience
+        work_experience_weight = self.answer_weights.get("experience", {}).get("work_experience", 0.0)
+        education_experience_weight = self.answer_weights.get("experience", {}).get("education_experience", 0.0)
+
+        max_experience_rating = 5  # Maximum rating value for both work and education experience
+
+        normalized_work_experience = self.experience[0] / max_experience_rating
+        normalized_education_experience = self.experience[1] / max_experience_rating
+
+        experience_preferences = {
+            "work_experience": normalized_work_experience * work_experience_weight,
+            "education_experience": normalized_education_experience * education_experience_weight
+        }
+        # Add programming type preferences with weights
+        programming_type_preferences = {
+            programming_type: self.programming_type.count(programming_type) * self.answer_weights.get("programming_type", {}).get("selected_types", 0.0)
+            for programming_type in self.programming_type
+        }
+        # Add specific language preferences with weights
+        specific_language_preferences = {
+            language: self.specific_languages.count(language) * self.answer_weights.get("specific_languages", {}).get("selected_languages", 0.0)
+            for language in self.specific_languages
+        }
+
+        # Combine occupation group preferences and experience preferences
+        user_preferences = {}
+        user_preferences.update(occupation_group_preferences)
+        user_preferences.update(experience_preferences)
+        user_preferences.update(programming_type_preferences)
+        user_preferences.update(specific_language_preferences)
+
+        return user_preferences
+    
+    #Creates a dictionary of occupation profiles, storing their values which will be used for matching.
     def compute_occupation_profiles(self):
-        # Implement the computation of occupation profiles from the data
-        pass
+        #Function to get the % where experience_required is True for each occupation
+        def compute_proportion_experience_required(occupation):
+            total_entries = len(self.data[self.data['occupation'] == occupation])
+            if total_entries == 0:
+                return 0.0
+            experience_required_entries = len(self.data[(self.data['occupation'] == occupation) & (self.data['experience_required'])])
+            proportion_experience_required = experience_required_entries / total_entries
+            return proportion_experience_required
+        def count_str(data, column_name, search_string):
+            pattern = fr"(?i)(?<!\S){re.escape(search_string)}(?!\S)"
+            filtered_data = data[data[column_name].notna() & data[column_name].str.contains(pattern, regex=True)]
+            count = filtered_data[column_name].str.contains(pattern, regex=True).sum()
+            return count
+        #Check how well each occupation matches with the programming types
+        def compute_word_counts(data, occupation, word_list):
+            occupation_data = data[data['occupation'] == occupation]
+            count = 0
+            for word in word_list:
+                count += count_str(occupation_data, 'annonstext', word)
+            return count
+        #Word lists for matching
+        web_development_words = ['html','css','javascript','front-end','frontend','user interface','ui','user experience','ux','användarupplevelse','design','web','website',
+            'hemsida','web application','web applications','seo','e-commerce','wordpress','google','Facebook']
+        general_purpose_programming_words = ['java', 'python', 'c++', 'ruby']
+        databases_data_manipulation_words = ['sql', 'database', 'query', 'data']
+        scientific_mathematical_computing_words = ['math', 'statistics', 'simulation']
+        not_interested_programming_words = ['programming', 'coding', 'software', 'development']
+
+
+        #Creates an entry for each occupation and calls the appropriate functions for them
+        occupation_profiles = {}
+        for occupation in self.data['occupation']:
+            if occupation not in occupation_profiles:
+                occupation_entries = len(self.data[self.data['occupation'] == occupation])
+                profile = {
+                    'occupation_group': self.data.loc[self.data['occupation'] == occupation, 'occupation_group_name'].values[0],
+                    'proportion_experience_required': compute_proportion_experience_required(occupation),
+                    'Web Development': compute_word_counts(self.data, occupation, web_development_words) / occupation_entries,
+                    'General-Purpose Programming': compute_word_counts(self.data, occupation, general_purpose_programming_words) / occupation_entries,
+                    'Databases and Data Manipulation': compute_word_counts(self.data, occupation, databases_data_manipulation_words) / occupation_entries,
+                    'Scientific and Mathematical Computing': compute_word_counts(self.data, occupation, scientific_mathematical_computing_words) / occupation_entries,
+                    'Not interested in programming': compute_word_counts(self.data, occupation, not_interested_programming_words) / occupation_entries
+                }
+                occupation_profiles[occupation] = profile
+
+        return occupation_profiles
 
     def measure_similarity(self, user_preferences, occupation_profiles):
-        # Implement the calculation of similarity scores between user preferences and occupation profiles
-        pass
+        similarity_scores = {}  # Create an empty dictionary to store similarity scores
+        
+        for occupation, profile in occupation_profiles.items():
+            # Calculate the similarity score between user preferences and each occupation profile
+            # Use appropriate similarity metrics or algorithms to compare the two profiles
+            break# Store the similarity score in the similarity_scores dictionary
+        
+        return similarity_scores
 
     def rank_occupations(self, similarity_scores):
-        # Implement the ranking of occupations based on similarity scores
-        pass
+        ranked_occupations = []  # Create an empty list to store ranked occupations
+        
+        # Sort the occupations based on the similarity scores in descending order
+        # Append the occupations to the ranked_occupations list in the sorted order
+        
+        return ranked_occupations
 
     def get_top_n_recommendations(self, ranked_occupations, n):
-        # Implement the selection of top N occupations as recommendations
-        pass
+        top_n_recommendations = []  # Create an empty list to store the top N recommendations
+        
+        # Select the top N occupations from the ranked_occupations list
+        # Append the selected occupations to the top_n_recommendations list
+        
+        return top_n_recommendations
